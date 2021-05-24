49f9bedd31930c51e1b33a908cb345fe738ac97f
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-05-24 19:06:43 -0300

    Metodo que devuelve todos los articulos de la DB

--- controllers/article.js
+++ controllers/article.js
@@ -64,6 +64,30 @@ var controller = {
         return res.status(200).send({
             article: params
         });
+    },
+    getArticles: (req, res) => {
+        // tenemos que hacer find para sacar los datos de la DB
+        Article.find({}).sort('-_id').exec((err, articles) => {
+
+            if(err){
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al devolver los articulos !!!'
+                });
+            }
+
+            if(!articles){
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay articulos para mostrar !!!'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                articles
+            });
+        }); 
     }
 };// end controller
 
